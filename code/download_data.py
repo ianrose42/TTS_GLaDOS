c23ef89484e59d9761be8f7e0e5775903547baf9
Ian Rose <ian@ianrose.io>
2023-04-24 17:03:20 -0700

    added dl part

--- code/download_data.py
+++ code/download_data.py
@@ -2,36 +2,64 @@
 
 import sys
 import os
+from os import path
 import argparse
+from pathlib import Path
 from data_utils import *
 
 # default values
 default_url = 'https://theportalwiki.com/wiki/GLaDOS_voice_lines'
-wait_time = 15
+wait_time = 150
+fp = path.abspath(str(sys.modules['__main__'].__file__))
+default_out_dir = path.abspath(join(fp ,"../..", 'glados_data'))
 
 if __name__ == "__main__":
     # help messages
-    url_help = '''URL to download data from.
-                Default: {}'''.format(default_url)
-    wait_help = '''Time (in seconds) to wait before re-trying a given
-                file's download. Default is {} seconds.'''.format(wait_time)
+    url_help = """\
+        URL to download data from.
+        Default: 
+        {}
+        """.format(default_url)
+    wait_help = """\
+        Time (in seconds) to wait before re-trying a given file's download. 
+        Default:
+        {}
+        """.format(wait_time)
+    out_dir_help = """\
+        Directory to save data being downloaded.
+        Default: 
+        {}
+        """.format(default_out_dir)
+
     ## args
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        )
     # url for download
     parser.add_argument("-u", "--url", 
                         help=url_help,
                         default=default_url,
-                        required=True)
+                        required=False)
     # dir to save data
     parser.add_argument('-o', '--out_dir',
-                        help='Directory to save data',
-                         required=True)
+                        help=out_dir_help,
+                        default=default_out_dir,
+                        required=False)
     # time-out pause
     parser.add_argument('-w', '--wait',
                         help=wait_help,
-                        default=15,
+                        default=wait_time,
                         required=False)
 
+    # get the args given to command line
     args = parser.parse_args()
-    if not args.help:
-        print("hello world!")
+    print()
+    # arg values to variables
+    base_url = args.url
+    out_dir = args.out_dir
+    wait_time = args.wait
+
+    # download with given variables
+    download_data(data_url=base_url,
+                out_path=out_dir,
+                wait_time=wait_time)
